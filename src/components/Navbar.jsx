2da6e65209791ae2e3f30c1e9a956aec5034b723
Peter Loft <peterloft@Peters-MacBook-Pro.local>
2025-05-07 15:16:43 -0700

    update for completion of first two steps for navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const Navbar = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+
+
+	return (
+		<div className="w-full h-16 md:h-20 flex items-center justify-between">
+			{/* logo */}
+			<div className="flex items-center gap-4 text-2xl font-bold">
+				<img src="/logo.png" alt="logo" className="w-8 h-8" />
+				<span className="">Lofty Blog</span>
+			</div>
+			{/* mobile menu */}
+			<div className="md:hidden text-xl cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
+				{
+					isOpen
+						? "X"
+						: "☰"
+				}
+			</div>
+			{/* mobile link list */}
+			<div className={`w-full h-screen flex flex-col items-center justify-center bg-white fixed top-16 md:top-20 left-0 z-10 transition-all duration-300 ease-in-out ${
+				isOpen
+					? "-translate-x-0"
+					: "translate-x-full"
+			}`}>
+				TEST
+			</div>
+
+			{/* desktop menu */}
+			<div className="hidden md:flex">D</div>
+
+
+		</div>
+	);
+}
+
+export default Navbar;
